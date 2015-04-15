38faff2eddcb2e6f73916d03a9e7e2ac63213235
Fabricio Paes <fabriciopaes1@gmail.com>
2015-04-15 10:20:20 -0300

    UPDATED AVL

---
+++ AVLTreeMain.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "AVLHeader.h"
+
+int main()
+{
+    Node* T = NULL;
+    int select, key;
+    Node *y;
+    printf("######## Binary Search Tree - Fabricio Paes - UFRPE ##########\n");
+
+    do {
+        printf("\n(1) Insert Node \n(2) Remove Node \n(3) Print Tree\n(4) Tree Minimum\n(5) Tree Maximum\n(6) Successor\n(7) Predecessor\n(0) Exit\n");
+        scanf("%d", &select);
+
+        switch(select){
+        case 0:
+            exit(0);
+        case 1:
+            printf("Insert the key(number): ");
+            scanf("%d", &key);
+            Node* newNode = malloc(sizeof(Node));
+            newNode->key = key;
+            newNode->leftSon = newNode->father = newNode->rightSon = NULL;
+            T = insert(T, newNode);
+            printf("\nInserted.\n");
+            break;
+        case 2:
+            printf("Insert the key: ");
+            scanf("%d", &key);
+            Node* toRemove = search(T, key);
+            if (toRemove == NULL)
+            {
+                printf("\n404 Not Found.\n");
+                break;
+            }
+            T = removeNode(T, toRemove);
+            printf("\nRemoved\n");
+            break;
+        case 3:
+            printf("\nPrinted Tree: \n");
+            printTree(T);
+            break;
+        case 4:
+            y = minimum(T);
+            if (y == NULL)
+            {
+                printf("\nEmpty Tree.\n");
+                break;
+            }
+            printf("\nMinimum of the tree: %d\n", y->key);
+            break;
+        case 5:
+            y = maximum(T);
+            if (y == NULL) {
+                printf("\nEmpty Tree.\n");
+                break;
+            }
+            printf("\nMaximum of the tree: %d\n", y->key);
+            break;
+        case 6:
+            printf("Insert the key: ");
+            scanf("%d", &key);
+            y = search(T, key);
+            y = successor(y);
+            if (y == NULL)
+            {
+                printf("\n404 Not Found.\n");
+                break;
+            }
+            printf("Successor of %d: %d\n", key, y->key);
+            break;
+        case 7:
+            printf("Insert the key: ");
+            scanf("%d", &key);
+            y = search(T, key);
+            y = predecessor(y);
+            if (y == NULL)
+            {
+                printf("\n404 Not Found.\n");
+                break;
+            }
+            printf("Predecessor of %d: %d\n", key, y->key);
+            break;
+        }
+    } while (select != 0);
+    return 0;
+}
--- README.txt
+++ README.txt
@@ -1,15 +1,21 @@
-README.txt Specifications
+README.txt Specifications 1.0
 
 
 BSTree = Binary Search Tree (Árvores de Busca Binária)
 AVLTree = Árvore AVL
 
+						~/ÁRVORE DE BUSCA BINÁRIA GERAL/~
 O código foi totalmente escrito em C e em ambiente Windows usando a IDE Code Blocks.
 
 Pode-se executar o arquivo 'BSTree.exe' para verificar o resultado da compilação do código 'BSTree.c' da Árvore de Busca Binária. Ou, para compilá-lo:
 	gcc -o BSTree.exe BSTree.c
 
-Para a árvore AVL, encontra-se um arquivo do tipo header onde estão declarados todos os métodos necessários, em conjunto com a estrutura de cada nó. Pode-se executar o arquivo AVLTree.exe para visualizar o resultado da compilação ou abrir os AVLTree.c e AVLHeader.h em uma IDE no mesmo projeto e executá-lo. Ou, para compilá-lo:
-	gcc -o AVLTree.exe AVLTree.c AVLTree.h
 
+						~/MAKEFILE ÁRVORE AVL/~
+
+O makefile para os arquivos da árvore AVL foi feito para compilação em Linux. No terminal, basta entrar no diretório onde os arquivos AVLHeader.h, AVLTree.c, AVLTreeMain.c e AVLMakefile.txt estão localizados (todos precisam estar na mesma pasta) e, então, executar o comando: 
+	make -f AVLMakefile.txt
+
+Para executá-lo basta digitar o comando (enquanto na mesmo diretório):
+	./avlTree
  
